f6e9eabd92bb177b27d8bdaa9714daa20300b719
Sina Osooli Nejad <48066919+sinaon13@users.noreply.github.com>
2026-02-16 23:23:14 +0330

    changed fifo bpf

--- scx_fifo.bpf.c
+++ scx_fifo.bpf.c
@@ -1,85 +1,64 @@
-/*
- * scx_fifo.bpf.c - A FIFO scheduler with Statistics (API Fixed)
- */
-
-#include "vmlinux.h"
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
 #include <scx/common.bpf.h>
 
 char _license[] SEC("license") = "GPL";
 
-struct task_stats {
-    u64 total_run_ns;
-    u64 total_wait_ns;
-    u64 nr_switches;
-    u64 last_enq_ts;
-    u64 last_run_ts;
-    char comm[16];
-};
+UEI_DEFINE(uei);
 
 struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 10240);
-    __type(key, u32);
-    __type(value, struct task_stats);
-} stats_map SEC(".maps");
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u64));
+	__uint(max_entries, 2);
+} stats SEC(".maps");
 
-// 5 seconds slice to ensure strict FIFO
-const volatile u64 fifo_slice_ns = 5000000000ULL; 
-
-void BPF_STRUCT_OPS(fifo_enable, struct task_struct *p)
+static __always_inline void stat_inc(u32 idx)
 {
-    u32 pid = p->pid;
-    struct task_stats stats = {};
-    bpf_probe_read_kernel_str(stats.comm, sizeof(stats.comm), p->comm);
-    bpf_map_update_elem(&stats_map, &pid, &stats, BPF_NOEXIST);
+	u64 *cnt = bpf_map_lookup_elem(&stats, &idx);
+	if (cnt)
+		(*cnt)++;
+}
+
+s32 BPF_STRUCT_OPS(fifo_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
+{
+	bool is_idle = false;
+	s32 cpu;
+
+	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
+
+	if (is_idle) {
+		stat_inc(0);
+		scx_bpf_dispatch(p, SCX_DSQ_LOCAL, SCX_SLICE_INF, 0);
+	}
+
+	return cpu;
 }
 
 void BPF_STRUCT_OPS(fifo_enqueue, struct task_struct *p, u64 enq_flags)
 {
-    u32 pid = p->pid;
-    struct task_stats *stats = bpf_map_lookup_elem(&stats_map, &pid);
-    
-    if (stats) {
-        stats->last_enq_ts = bpf_ktime_get_ns();
-    }
-    
-    //  scx_bpf_dsq_insert instead of scx_bpf_dispatch
-    scx_bpf_dsq_insert(p, SCX_DSQ_GLOBAL, fifo_slice_ns, enq_flags);
+	stat_inc(1);
+	scx_bpf_dispatch(p, SCX_DSQ_GLOBAL, SCX_SLICE_INF, enq_flags);
 }
 
-void BPF_STRUCT_OPS(fifo_running, struct task_struct *p)
+void BPF_STRUCT_OPS(fifo_dispatch, s32 cpu, struct task_struct *prev)
 {
-    u32 pid = p->pid;
-    struct task_stats *stats = bpf_map_lookup_elem(&stats_map, &pid);
-    u64 now = bpf_ktime_get_ns();
-
-    if (stats) {
-        if (stats->last_enq_ts) {
-            stats->total_wait_ns += (now - stats->last_enq_ts);
-        }
-        stats->nr_switches++;
-        stats->last_run_ts = now;
-    }
+	scx_bpf_consume(SCX_DSQ_GLOBAL);
 }
 
-void BPF_STRUCT_OPS(fifo_stopping, struct task_struct *p, bool runnable)
+s32 BPF_STRUCT_OPS_SLEEPABLE(fifo_init)
 {
-    u32 pid = p->pid;
-    struct task_stats *stats = bpf_map_lookup_elem(&stats_map, &pid);
-    u64 now = bpf_ktime_get_ns();
-
-    if (stats && stats->last_run_ts) {
-        stats->total_run_ns += (now - stats->last_run_ts);
-    }
+	return 0;
 }
 
-SEC(".struct_ops.link")
-struct sched_ext_ops fifo_ops = {
-    .enable     = (void *)fifo_enable,
-    .enqueue    = (void *)fifo_enqueue,
-    .running    = (void *)fifo_running,
-    .stopping   = (void *)fifo_stopping,
-    .name       = "scx_fifo_stats",
-};
+void BPF_STRUCT_OPS(fifo_exit, struct scx_exit_info *ei)
+{
+	UEI_RECORD(uei, ei);
+}
+
+SCX_OPS_DEFINE(fifo_ops,
+	       .select_cpu	= (void *)fifo_select_cpu,
+	       .enqueue		= (void *)fifo_enqueue,
+	       .dispatch	= (void *)fifo_dispatch,
+	       .init		= (void *)fifo_init,
+	       .exit		= (void *)fifo_exit,
+	       .flags		= SCX_OPS_SWITCH_PARTIAL,
+	       .name		= "fifo");
